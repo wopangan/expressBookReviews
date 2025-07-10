ff2382e4aeb2490fe3473986c122388103680d1d
wopangan <webster_pangan@yahoo.com>
2025-07-10 14:21:20 +0800

    add route for book search by title (not implemented)

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,25 +10,47 @@ public_users.post("/register", (req,res) => {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Task 1: Get the book list available in the shop
+public_users.get('/books',function (req, res) {
+  res.send(JSON.stringify(books))
 });
 
-// Get book details based on ISBN
+// Task 2: Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  const id = req.params.isbn;
+
+  // checks if id is provided and if the book exists in the 'books'.
+  if (id && books[id]) {
+    const bookDetails = books[id]
+    return res.send(bookDetails);
+  } else {
+    res.status(404).json({ message: "No book found"})
+  }
+});
   
-// Get book details based on author
+// Task 3: Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const keys = Object.keys(books); // get all the keys
+  let results = []; // to collec and store all the books written by the searched author
+
+  // loop through the books and check for matching author
+  keys.forEach(key => {
+    if (books[key].author.toLowerCase() === author.toLowerCase()) {
+      results.push(books[key]);
+    }
+  })
+
+  // check if author exists
+  if (results.length > 0 ) {
+    res.send(results)
+  } else {
+    res.status(404).json({message: "Author not found"});
+  }
 });
 
-// Get all books based on title
+// To be continued...
+// Task 4: Get all books based on title 
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
