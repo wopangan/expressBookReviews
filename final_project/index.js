293f041ccbc1c529f723f1025303f366315fdb3c
wopangan <webster_pangan@yahoo.com>
2025-07-10 14:12:22 +0800

    initial commit

--- final_project/index.js
+++ final_project/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const session = require('express-session')
@@ -6,12 +7,36 @@ const genl_routes = require('./router/general.js').general;
 
 const app = express();
 
+const JWT_SECRET = process.env.JWT_SECRET;
+const SESSION_SECRET = process.env.SESSION_SECRET;
+
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer",session({
+    secret: SESSION_SECRET,
+    resave: true, 
+    saveUninitialized: true
+}));
 
+// Update the authentication code with session authorization feature
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+
+    // check if user has logged in and has valid access token
+    if (req.session.authorization) {
+        let token = req.session.authorization['accessToken'];
+
+        // verify jwt token
+        jwt.verify(token, JWT_SECRET, (err, res) => {
+            if (!err) {
+                res.user = user;
+                next();
+            } else{
+                return res.status(404).json({ message: "User not authenticated."});
+            }   
+        });
+    } else {
+        return res.status(404).json({ message: "User not logged in."});
+    }
 });
  
 const PORT =5000;
