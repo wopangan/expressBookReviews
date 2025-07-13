3bf6e13670b0c8a734503b4cd32fbff2d951ac19
wopangan <webster_pangan@yahoo.com>
2025-07-13 15:17:07 +0800

    implement book routes using Promises and async/await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,70 +27,103 @@ public_users.post("/register", (req,res) => {
 
 });
 
+// Task 10: Modify by using Promise callbacks or async-await with Axios.
 // Task 1: Get the book list available in the shop
-public_users.get('/books',function (req, res) {
-  res.send(JSON.stringify(books))
+public_users.get('/books', function (req, res) {
+  new Promise((resolve) =>{
+    setTimeout(() => { 
+      resolve(books) 
+    }, 500);
+  })
+  .then((bookList) => {
+    res.json(bookList);
+  })
+  .catch(() => {
+    res.status(500).json({ message: "Failed to fetch books data."})
+  })
 });
 
+// Task 11: Get book details based on ISBN using Promises callbacks or async-await with Axios
 // Task 2: Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', function (req, res) {
   const id = req.params.isbn;
-
-  // checks if id is provided and if the book exists in the 'books'.
-  if (id && books[id]) {
-    const bookDetails = books[id]
-    return res.send(bookDetails);
-  } else {
-    res.status(404).json({ message: "No book found"})
-  }
-});
   
+  new Promise((resolve) => {
+    setTimeout(() => { 
+      resolve(books) 
+    }, 500);
+  })
+  .then(() => {
+    if (id && books[id]) {
+      const book = books[id]
+      res.json(book);
+    } else {
+      res.status(404).json({ message: "Book not found."})
+    }
+  })
+  .catch(() => {
+    res.status(500).json({ message: "Failed to fetch book data."})
+  })
+});
+
+// Task 12: Get book details based on author using Promise callbacks or async-await with Axios
 // Task 3: Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
   const author = req.params.author;
   const keys = Object.keys(books); // get all the keys
   let results = []; // to collect and store all the books written by the searched author
 
-  // loop through the books and check for matching author
-  keys.forEach(key => {
-    if (books[key].author.toLowerCase() === author) {
-      results.push(books[key]);
+  try {
+    const bookList = await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(books);
+      }, 500)
+    });
+    // loop through the books and check for matching author
+    keys.forEach(key => {
+      if (bookList[key].author.toLowerCase() === author.toLowerCase()) {
+        results.push(bookList[key]);
+      }
+    });
+    // check if author exists
+    if (results.length > 0 ) {
+      res.send(results);
+    } else {
+      res.status(404).json({message: "Author not found"});
     }
-  })
 
-  // check if author exists
-  if (results.length > 0 ) {
-    res.send(results)
-  } else {
-    res.status(404).json({message: "Author not found"});
+  } catch {
+    res.status(500).json({ message: "Failed to fetch book data." });
   }
 });
 
-// Task 4: Get all books based on title 
-public_users.get('/title/:title',function (req, res) {
-  const title = req.params.title.toLowerCase();
-  // const keys = Object.keys(books);
+// Task 13: Get book details based on title using Promise callbacks or async-await with Axios
+// Task 4: Get book details based on title 
+public_users.get('/title/:title', async function (req, res) {
+  const title = req.params.title;
   let results = [];
 
-  // keys.forEach(key => {
-  //   if (books[key].title.toLowerCase() === title) {
-  //     results.push(books[key]);
-  //   }
-  // });
+  try {
+    const bookList = await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(books);
+      }, 500);
+    });
 
-  // in other approach
-
-  for (const key in books) {
-    if (books[key].title.toLowerCase() === title) {
-      results.push(books[key])
+    for (const key in bookList) {
+      if (bookList[key].title.toLowerCase() === title.toLowerCase()) {
+        results.push(bookList[key]);
+      } 
     }
-  }
 
-  // check if title of the books exists
-  if (results.length > 0) {
-    res.send(results);
-  } else {
-    res.status(404).json({ message: "Title not found"});
+    // check if book title exists
+    if (results.length > 0) {
+      res.json(results);
+    } else {
+      res.status(404).json({ message: "Title not found"});
+    }
+  } catch {
+    res.status(500).json({ message: "Failed to fetch book data." });
   }
 });
 
